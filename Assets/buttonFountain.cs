fdaff72bac43e391adc2e6aa918ecde95cf4625f
sterlingtr <sterlinr@usc.edu>
2017-09-23 12:49:11 -0700

    Add files via upload

---
+++ Assets/buttonFountain.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class buttonFountain : MonoBehaviour {
+
+    public ToggleFountain fountain;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.collider.tag.Equals("Player"))
+        {
+            fountain.Toggle();
+        }
+    }
+}
---
+++ Assets/killObject.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class killObject : MonoBehaviour {
+
+    public Transform spawnLocation;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag.Equals("Player"))
+        {
+            other.gameObject.transform.position = spawnLocation.position;
+        }
+    }
+}
